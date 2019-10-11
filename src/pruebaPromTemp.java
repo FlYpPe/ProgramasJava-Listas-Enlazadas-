e6983672221ed26306e96e0e7ce8873a3950faed
Felipe Marquez <flype360360@gmail.com>
2019-10-11 15:33:23 -0500

    Se comenzo a trabajar

---
+++ src/pruebaPromTemp.java
@@ -0,0 +1,9 @@
+
+public class pruebaPromTemp {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/pruebaTemperaturas.java
@@ -0,0 +1,10 @@
+
+public class pruebaTemperaturas {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
